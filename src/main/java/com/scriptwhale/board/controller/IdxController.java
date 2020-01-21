ee27c90934994f81ee3bb71eef661cfd391c4008
eager <scriptwhale>
2020-01-21 17:22:55 +0900

    Controller Error

--- src/main/java/com/scriptwhale/board/controller/IdxController.java
+++ src/main/java/com/scriptwhale/board/controller/IdxController.java
@@ -1,22 +1,20 @@
 package com.scriptwhale.board.controller;
 
-import com.scriptwhale.board.repository.UsersDAO;
+import com.scriptwhale.board.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.scriptwhale.board.repository.UsersDAOImpl;
-
 @Controller
 public class IdxController {
 	
 	@Autowired
-	private UsersDAO usersDAO;
+	private UsersService usersService;
 
 	@RequestMapping(value = "/")
 	public String index(Model model) {
-		model.addAttribute("userId", usersDAO.selectUserId("scriptwhale"));
+		model.addAttribute("userId", usersService.selectUserId("scriptwhale"));
 		return "/index";
 	}
 	
---
+++ webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: eager
+  Date: 2020/01/21
+  Time: 5:03 오후
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>${ usersId } page </title>
+</head>
+<body>
+    <p>${ usersId }</p>
+    <p>test page</p>
+</body>
+</html>
